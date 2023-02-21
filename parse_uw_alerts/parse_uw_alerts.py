37ee59393907fdcdd3530100f21eed48a925c8d4
jmic94 <johnmichael1994@icloud.com>
2023-02-20 21:53:33 -0800

    Parsed UW Alerts text file

--- parse_uw_alerts/parse_uw_alerts.py
+++ parse_uw_alerts/parse_uw_alerts.py
@@ -7,7 +7,7 @@ import time
 import re
 import pandas as pd
 import openai
-# from transformers import GPT2Tokenizer
+from transformers import GPT2Tokenizer
 
 def prompt_gpt(lines, alert_start, alert_end):
     """
@@ -31,27 +31,28 @@ def prompt_gpt(lines, alert_start, alert_end):
         raise ValueError(
             "alert_end cannot be greater than the length of lines")
     print((alert_start, alert_end))
-    gpt_task = ('Extract a table with date, report time, incident'
-                ' time, location, incident category, and summarized'
+    gpt_task = ('Extract a markdown table with the columns date, report time,'
+                ' incident time, location, incident category, and summarized'
                 ' alert message from the alert messages below.\n'
                 'Text: """')
     alert_chunk = '\n'.join(lines[alert_start:alert_end])
     gpt_prompt = ''.join([gpt_task, alert_chunk])
     gpt_prompt += '\n"""'
-    # tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
-    # n_tokens = len(tokenizer(gpt_prompt)['input_ids'])
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    n_tokens = len(tokenizer(gpt_prompt)['input_ids'])
     response = openai.Completion.create(
         engine="text-davinci-003",
         prompt=gpt_prompt,
         temperature=0,
-        max_tokens=3200)
-    gpt_lines = io.StringIO(response['choices'][0]['text']).readlines()
-    for i, line in enumerate(gpt_lines):
-        if line.startswith('Date'):
-            start_line = i
-    gpt_output = ''.join(gpt_lines[start_line:])
+        max_tokens=4097-n_tokens)
+    print(response['choices'][0]['text'])
+    gpt_table = pd.read_table(
+        io.StringIO(response['choices'][0]['text']), sep='|', \
+            skipinitialspace=True, header=0
+            ).dropna(axis=1, how='all').rename(
+                columns=lambda x: x.strip()).iloc[1:]
     time.sleep(10)
-    return pd.read_table(io.StringIO(gpt_output), sep='|')
+    return gpt_table
 
 def parse_txt_data(filepath):
     """
@@ -70,15 +71,16 @@ def parse_txt_data(filepath):
     if re.search(r'\.txt$', filepath) is None:
         raise ValueError("filepath must have a .txt extension")
 
-    clean_data = None
     with open(filepath, encoding='UTF-8') as file:
         lines = file.readlines()
         alert_chunk_start = None
         alert_chunk_end = None
-        for i, line in enumerate(lines):
+        for i, line in enumerate(lines[452:]):
+            clean_data = pd.read_csv('./data/uw_alerts_clean.csv', index_col=False)
             if i == len(lines) - 1:
                 table = prompt_gpt(lines, alert_chunk_start, i)
                 clean_data = pd.concat([clean_data, table], ignore_index=True)
+                clean_data.to_csv('./data/uw_alerts_clean.csv', index=False)
             date_check = re.search(r'^[A-z]+\s\d{1,2},\s\d{4}\n$', line)
             if date_check:
                 if alert_chunk_start is None:
@@ -89,13 +91,14 @@ def parse_txt_data(filepath):
                         lines, alert_chunk_start, alert_chunk_end)
                     clean_data = pd.concat(
                         [clean_data, table], ignore_index=True)
+                    clean_data.to_csv('./data/uw_alerts_clean.csv', index=False)
                     alert_chunk_start = i
+            
     return clean_data
 
 if __name__ == "__main__":
-    OPENAI_API_KEY =  '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'
+    OPENAI_API_KEY =  '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'
     FILEPATH = './data/UW_Alerts_2018_2022.txt'
     OUT_FILEPATH = './data/uw_alerts_clean.csv'
     openai.api_key = OPENAI_API_KEY
-    text_dataframe = parse_txt_data(FILEPATH)
-    text_dataframe.to_csv(OUT_FILEPATH, index=False)
+    parse_txt_data(FILEPATH)
