627bdbd2493ee326f8307f62c00256ab20d6ace0
mtqiao4 <mtqiao@uw.edu>
2023-02-22 18:34:32 -0800

    added functionality for comparing to previous alert using regex

--- app/scraper.py
+++ app/scraper.py
@@ -1,11 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
+import re
 
-#scrapes UW alerts for all the text for each alert. 
-def scrape_uw_alerts():
+def scrape_uw_alerts(prev_alert_list):
+    newest_alert_list = []
+    date_counter = 0
+    same_alert_flag = True
+    pattern = "(January|February|March|April|May|June|July|August|September|October|November|December)\s+\d{1,2},\s+\d{4}"
+
     URL = "https://emergency.uw.edu/"
     page = requests.get(URL)
     soup = BeautifulSoup(page.content,"html.parser")
     mainContent = soup.find(id="main_content")
     p_tags = mainContent.find_all('p')
-    return p_tags
+    for p in p_tags:
+        if re.match(pattern,p.text):
+            date_counter+=1
+        if date_counter<2:
+            newest_alert_list.append(p)
+
+    if(len(newest_alert_list) != len(prev_alert_list)):
+        same_alert_flag == False
+    else:
+        for i in range(len(newest_alert_list)):
+            if newest_alert_list[i].text != prev_alert_list[i].text:
+                same_alert_flag = False
+            else:
+                pass
+    
+    if same_alert_flag == False:
+        return newest_alert_list
+    return None
+
+   
