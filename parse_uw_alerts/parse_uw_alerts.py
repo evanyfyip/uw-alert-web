b7f35d6abeb3023a8b6adb5145e04d6dc2f88274
jmic94 <johnmichael1994@icloud.com>
2023-02-25 01:43:49 -0800

    Reran GPT extraction with more specific date instructions

--- parse_uw_alerts/parse_uw_alerts.py
+++ parse_uw_alerts/parse_uw_alerts.py
@@ -4,9 +4,7 @@ key incident information in a tabular format.
 """
 import io
 import time
-import string
 import re
-from datetime import datetime
 import pandas as pd
 import openai
 from transformers import GPT2Tokenizer
@@ -30,7 +28,9 @@ def prompt_gpt(lines):
         raise ValueError("lines must be at least length 1")
     gpt_task = ('Extract a markdown table with the columns Date, Report Time,'
                 ' Incident Time, Incident Address, Incident Category, and'
-                ' Incident Summary from the following alert message\n'
+                ' Incident Summary from the following alert message.'
+                ' Make sure the Date is in mm/dd/yyy format and times are'
+                ' in 12-hour format.'
                 'Text: """')
     alert_chunk = '\n'.join(lines)
     gpt_prompt = '\n'.join([gpt_task, alert_chunk])
@@ -48,12 +48,12 @@ def prompt_gpt(lines):
             skipinitialspace=True, header=0, index_col=False)
     gpt_table.rename(columns=lambda x: x.strip(), inplace=True)
     gpt_table = gpt_table.iloc[1:]
-    column_names = ['Date', 'Report Time', 'Incident Time', 
+    column_names = ['Date', 'Report Time', 'Incident Time',
                     'Incident Address', 'Incident Category',
                     'Incident Summary']
     gpt_table = gpt_table.loc[:, column_names]
     for column in column_names:
-        gpt_table[column] = gpt_table[column].str.strip()
+        gpt_table[column] = gpt_table[column].astype(str).str.strip()
     time.sleep(10)
     return gpt_table
 
@@ -84,65 +84,31 @@ def parse_txt_data(filepath, out_filepath):
         lines = file.readlines()
         alert_chunk_start = None
         alert_chunk_end = None
-        start = 0
+        start = 1199
         for i, line in enumerate(lines[start:]):
             if i + start == len(lines) - 1:
                 print((alert_chunk_start+start, i+start))
                 table = prompt_gpt(lines[(alert_chunk_start+start):(i+start)])
                 clean_data = pd.read_csv(out_filepath, index_col=False)
                 clean_data = pd.concat([clean_data, table], ignore_index=True)
-                print(list(clean_data.columns))
+                # print(list(clean_data.columns))
                 clean_data.to_csv(out_filepath, index=False)
             date_check = re.match(r'^[A-z]+\s\d{1,2},\s\d{4}\n$', line)
             if date_check:
                 if alert_chunk_start is None:
                     alert_chunk_start = i
                 else:
-                    alert_chunk_end = i - 1
+                    alert_chunk_end = i
                     print((alert_chunk_start+start, alert_chunk_end+start))
                     table = prompt_gpt(
                         lines[(alert_chunk_start+start):(alert_chunk_end+start)])
                     clean_data = pd.read_csv(out_filepath, index_col=False)
                     clean_data = pd.concat(
                         [clean_data, table], ignore_index=True)
-                    print(list(clean_data.columns))
+                    # print(list(clean_data.columns))
                     clean_data.to_csv(out_filepath, index=False)
                     alert_chunk_start = i
     return clean_data
-
-def clean_date(date_string):
-    """
-    Arguments:
-        date_string - string containing date.
-    Returns:
-        a tuple (year, month, day).
-    Exceptions:
-        date_string must be a string.
-        ValueError will be thrown otherwise.
-    """
-    if not isinstance(date_string, str):
-        raise ValueError("date_string must be a string")
-    
-    date_abbr = {'jan': 1, 'feb': 2, 'mar': 3, 'apr': 4, 
-                 'may': 5, 'jun': 6, 'jul': 7, 'aug': 8, 
-                 'sep': 9, 'oct': 10, 'nov': 11, 'dec': 12}
-    if re.search('\d{2}/\d{2}/\d{4}', date_string):
-        date_object = datetime.strptime(date_string, '%m/%d/%Y')
-        return (date_object.year, date_object.month, date_object.day)
-    else:
-        for month, month_number in date_abbr.items():
-            if re.search(month, date_string, re.IGNORECASE):
-                month_num = month_number
-                break
-        year = re.search('\d{4}$', date_string)
-        if year:
-            year = int(year.group(0))
-        else:
-            year = None
-        day = re.sub('\d{4}$|[A-z]', '', date_string)
-        day = ''.join(['' if c in string.punctuation else c for c in day])
-        day = int(day.strip())
-        return (year, month_num, day)
 
 def clean_gpt_output(gpt_output='./data/uw_alerts_gpt.csv'):
     """
@@ -156,36 +122,28 @@ def clean_gpt_output(gpt_output='./data/uw_alerts_gpt.csv'):
         ValueError will be thrown otherwise.
     """
     if not isinstance(gpt_output, str):
-       if not isinstance(gpt_output, pd.DataFrame):
-           raise ValueError(
-               "gpt_output must be a filepath or Pandas DataFrame")
-       else:
-           gpt_data = gpt_output.copy()
+        if not isinstance(gpt_output, pd.DataFrame):
+            raise ValueError(
+                "gpt_output must be a filepath or Pandas DataFrame")
+        gpt_data = gpt_output.copy()
     else:
         if not re.search('.csv$', gpt_output):
             raise ValueError("gpt_output must be a .csv filepath")
-        else:
-            gpt_data = pd.read_csv(gpt_output, index_col=False)
-        
-    ymd = [clean_date(date) for date in gpt_data['Date'].values]
-    gpt_data['Year'] = [date[0] for date in ymd]
-    gpt_data['Month'] = [date[1] for date in ymd]
-    gpt_data['Day'] = [date[2] for date in ymd]
-    gpt_data[['Year']] = gpt_data[['Year']].fillna(method='ffill')
-    gpt_data['Date'] = pd.to_datetime(dict(year=gpt_data.Year,
-                                           month=gpt_data.Month,
-                                           day=gpt_data.Day))
+        gpt_data = pd.read_csv(gpt_output, index_col=False)
+
+    gpt_data['Date'] = pd.to_datetime(gpt_data['Date'],
+                                      infer_datetime_format=True)
     gpt_data['Date'] = gpt_data['Date'].dt.date
     gpt_data['Report Time'] = gpt_data['Report Time'].str.upper()
     gpt_data['Incident Time'] = gpt_data['Incident Time'].str.upper()
     gpt_data['Report Time'] = gpt_data['Report Time'].str.replace(
-        '\.|\s+', '', regex=True)
+        r'\.', '', regex=True)
     gpt_data['Incident Time'] = gpt_data['Incident Time'].str.replace(
-        '\.|\s+', '', regex=True)
+        r'\.', '', regex=True)
     print(gpt_data['Report Time'].values)
-     
+
 if __name__ == "__main__":
-    OPENAI_API_KEY =  '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'
+    OPENAI_API_KEY =  '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'
     FILEPATH = './data/UW_Alerts_2018_2022.txt'
     OUT_FILEPATH = './data/uw_alerts_gpt.csv'
     openai.api_key = OPENAI_API_KEY
