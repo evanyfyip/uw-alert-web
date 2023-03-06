7ba526b6b93463ed2f3467830cdd4ce5ae04d12a
mtqiao4 <mtqiao@uw.edu>
2023-03-06 10:42:40 -0800

    initial ajax setup

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
 import json
 import plotly
 import plotly.express as px
-
+import folium
 import sys
 import numpy as np
 import geopandas as gpd
@@ -23,6 +23,18 @@ app = Flask(__name__)
 def plot_folium_map():
     map = get_folium_map()
     return render_template('/base.html', map_html=map)
+
+@app.route('/update_map',methods=['POST'])
+def update_map():
+    new_data = request.form['text-input']
+    #TODO: combine all modules here to update the map
+    return new_data
+
+@app.route('/change_map')
+def change_map():
+    map = folium.Map(location=[51.5074, -0.1278], zoom_start=10)
+    map_html = map.get_root().render()
+    return map_html
 
 if __name__ == '__main__':
     app.run(debug=True)
