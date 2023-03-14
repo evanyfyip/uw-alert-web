283a62aebbdc8f2e11b6083e2b7be9a7cd5a162b
Evan Yip <evan@yipsite.net>
2023-03-14 16:19:56 -0700

    Included requirements txt file and heroku deployment files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn uw-alert-web.uw-alert-web:app
--- setup.py
+++ setup.py
@@ -1,23 +1,11 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="uw-alert-web",
     version="0.1",
     packages=find_packages(),
-    install_requires=[
-        'coverage',
-        'pyproj',
-        'folium==0.14.0',
-        'shapely==2.0.1',
-        'flask==2.2.2',
-        'numpy==1.24.2',
-        'pylint==2.16.2',
-        'geopandas==0.12.2',
-        'openai==0.26.5',
-        'transformers==4.26.1',
-        'requests==2.28.1',
-        'bs4',
-        'python-dotenv==0.21.0',
-        'googlemaps==4.10.0',
-    ],
+    install_requires=requirements,
 )
